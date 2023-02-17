6179b15b2364eb610c265ba19d5ddc06c561ee16
tanmaybonde <tybonde2002@gmail.com>
2023-02-17 19:57:53 +0530

    all code will added

---
+++ script.js
@@ -0,0 +1,11 @@
+function clearbox(){
+    document.getElementById("result").value="";
+}
+function display(value){
+    document.getElementById("result").value+=value;
+}
+function calculate(){
+    var a= document.getElementById("result").value;
+    var b=eval(a);
+    document.getElementById("result").value=b;
+}
